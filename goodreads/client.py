71f9e80996653480ff401049d2c97caf16eaca81
Tejesh Agrawal <tejeshagrawal@gmail.com>
2017-01-09 04:19:54 +0530

    Update client.py

--- goodreads/client.py
+++ goodreads/client.py
@@ -92,7 +92,7 @@ class GoodreadsClient():
 
             resp = self.request("book/show", {'id': book_id})
             resp1 = self.request("book/show", {'id': book_id})
-            return (GoodreadsBook(resp['book'], self).title)+(GoodreadsBook(resp1['book'], self).link)
+            return (GoodreadsBook(resp['book'], self).title)+(GoodreadsBook(resp1['book'], self).link)+'\n'
             
         elif isbn:
             resp = self.request("book/isbn", {'isbn': isbn})
